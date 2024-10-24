90b6cf249b1a9c0776005d19e7e7ad579aa3b1a9
Aniket Agnihotri <agnihotri.aniketagnihotri.anik@gmail.com>
2024-10-24 05:35:09 +0530

    Loader Successfully Updated

--- Obys Agency Clone/script.js
+++ Obys Agency Clone/script.js
@@ -8,7 +8,7 @@ tl.from(".line h1", {
 })
 
 
-tl.from("#line1-part1, .line h2",{
+tl.from("#line1-part1",{
     opacity:0,
     onStart:function(){
         var h5timer = document.querySelector("#line1-part1 h5")
@@ -23,9 +23,25 @@ setInterval(function(){
     }
 })
 
+tl.to('.line h2', {
+    animationName: "anime",
+    opacity:1
+})
 
 tl.to("#loader", {
     opacity:0,
-    duration: 0.4,
+    duration: 0.2,
     delay:4
+})
+
+tl.from("#page1",{
+    delay:0.2,
+    duration: 0.5,
+    y:1600,
+    opacity:0,
+    ease:power4
+})
+
+tl.to("#loader", {
+    display:"none"
 })
